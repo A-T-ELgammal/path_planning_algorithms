4c970cc689338104ba956df046184f5192892421
atelgammal <a.t.elgammal@gmail.com>
2024-05-22 12:39:42 +0300

    create minkowski_sum with functions(print 2D vector & remove duplicate coordinate)

--- Classic_algorithms/minkowski_sum.cpp
+++ Classic_algorithms/minkowski_sum.cpp
@@ -1,9 +1,71 @@
-// implementing Minkowski_sum algirthm on two triangle vectors
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
+// Print 2D vectors coordinate values
+void print2DVector(vector<vector<int>> vec)
+{
+    // Sorting the vector for grading purpose
+    sort(vec.begin(), vec.end());
+    for (int i = 0; i < vec.size(); ++i)
+    {
+        for (int j = 0; j < vec[0].size(); ++j)
+        {
+            cout << vec[i][j] << "  ";
+        }
+        cout << endl;
+    }
+}
+
+void delete_duplicate(vector<vector<int>> &C)
+{
+    sort(C.begin(), C.end());
+    vector<vector<int>> Cn;
+    for (int i = 0; i < C.size() - 1; i++)
+    {
+        if (C[i] != C[i + 1])
+        {
+            Cn.push_back(C[i]);
+        }
+    }
+    Cn.push_back(C[C.size() - 1]);
+    print2DVector(Cn);
+}
+
+vector<vector<int>> minkowski_sum(vector<vector<int>> A, vector<vector<int>> B)
+{
+    return B;
+}
+
 int main()
 {
+    // Define the coordinates of triangle A and B using 2D vectors
+    vector<vector<int>> A(3, vector<int>(2));
+    A = {
+        {1, 0},
+        {0, 1},
+        {0, -1},
+        {1, 0}};
+    vector<vector<int>> B(3, vector<int>(2));
+    B = {
+        {0, 0},
+        {1, 1},
+        {1, -1},
+    };
+    vector<vector<int>> S(4, vector<int>(2));
+    S = {{1, 2},
+         {2, 3},
+         {1, 2},
+         {3, 1}};
+    // Compute the minkowski sum of triangle A and B
+    // vector<vector<int>> C;
+    // C = minkowski_sum(A, B);
+
+    // // Print the resulting vector
+    delete_duplicate(S);
+    // print2DVector(B);
+
     return 0;
 }
