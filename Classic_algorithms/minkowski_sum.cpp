73c9c414db023bc157a0161427562116f7673fd4
atelgammal <a.t.elgammal@gmail.com>
2024-05-22 22:56:50 +0300

    create minkowski_sum & modify delete_duplicate & test result

--- Classic_algorithms/minkowski_sum.cpp
+++ Classic_algorithms/minkowski_sum.cpp
@@ -23,46 +23,40 @@ vector<vector<int>> delete_duplicate(vector<vector<int>> &C)
     sort(C.begin(), C.end());
     for (int i = 0; i < C.size() - 1; i++)
     {
-        if (C[i] == C[i + 1])
-            C.erase(C.begin() + i);
+        while (C[i] == C[i + 1])
+            C.erase(C.begin() + i + 1);
     }
     return C;
 }
 
 vector<vector<int>> minkowski_sum(vector<vector<int>> A, vector<vector<int>> B)
 {
-    return B;
+    vector<vector<int>> sum;
+    for (int i = 0; i < A.size(); i++)
+    {
+        for (int j = 0; j < B.size(); j++)
+        {
+            sum.push_back({A[i][0] + B[j][0], A[i][1] + B[j][1]});
+        }
+    }
+    return delete_duplicate(sum);
 }
 
 int main()
 {
-    // Define the coordinates of triangle A and B using 2D vectors
     vector<vector<int>> A(3, vector<int>(2));
     A = {
         {1, 0},
         {0, 1},
         {0, -1},
-        {1, 0}};
+    };
+
     vector<vector<int>> B(3, vector<int>(2));
     B = {
         {0, 0},
         {1, 1},
         {1, -1},
     };
-    vector<vector<int>> S(4, vector<int>(2));
-    S = {{1, 2},
-         {2, 3},
-         {1, 2},
-         {3, 1}};
-
-    // Compute the minkowski sum of triangle A and B
-    // vector<vector<int>> C;
-    // C = minkowski_sum(A, B);
-
-    // // Print the resulting vector
-    // delete_duplicate(S);
-    // print2DVector(B);
-    print2DVector(delete_duplicate(S));
-
+    print2DVector(minkowski_sum(A, B));
     return 0;
 }
